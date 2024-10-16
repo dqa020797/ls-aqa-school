6c0cf509ee6526f0dae9350ca36b9f2c84c87df3
anduser <dqa020792@gmail.com>
2024-10-16 23:52:07 +0300

    Задание №2 часть 2. .Потренироваться использовать функции, классы, объекты и методы. Добавлен класс OddTask , используется ввод через консоль для проверки чисел.

---
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/OddTask.java
@@ -0,0 +1,22 @@
+package ru.ls.qa.school.core;
+
+import java.util.Scanner;
+
+public class OddTask {
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        {
+            System.out.println("Введите число и получи результат: false - чётное! true - нечётное : ");
+        }
+
+        while (true) {
+            int number = console.nextInt();
+            boolean result = isOdd(number);
+            System.out.println("Число " + number + "." + " Ваш результат: " + result);
+        }
+    }
+}
