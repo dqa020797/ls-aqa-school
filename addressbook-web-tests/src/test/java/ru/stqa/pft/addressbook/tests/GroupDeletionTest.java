67494449e7ecafa1dfca1f787e11d32e42c7a3d8
Huawei <dqa020797@gmail.com>
2025-03-12 22:59:32 +0300

    №9. Перенос метода в pages. Удаление лишнего с test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,27 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTest extends TestBase {
 
+    @BeforeEach
+    public void ensurePreconditions() {
+        pages.home()
+             .goToGroupsPage()
+             .ensureGroupExists();
+    }
+
     @Test
     public void deleteGroupTest() {
-        if (app.getGroupHelper().isGroupPresent()) {
-            System.out.println("Группа найдена, удаляем...");
-            pages.home()
-                 .goToGroupsPage()
-                 .selectGroup()
-                 .deleteGroup();
-        } else {
-            System.out.println("Группы нет, создаем и удаляем.");
-            pages.home()
-                 .goToGroupsPage()
-                 .createNewGroup()
-                 .fillGroupForm(new GroupData("Test Group", "Test Header", "Test Footer"))
-                 .submitCreate()
-                 .selectGroup()
-                 .deleteGroup();
-        }
+        pages.home()
+             .goToGroupsPage()
+             .selectGroup()
+             .deleteGroup();
     }
 }
